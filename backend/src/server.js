29ea335268dd5e017537a9677a148a8c541ff576
mohamed-kh11 <mkhaled11@outlook.com>
2025-08-25 14:52:23 +0300

    localhost

--- backend/src/server.js
+++ backend/src/server.js
@@ -22,7 +22,7 @@ const app = express();
 app.use(helmet());
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "*", // allow your frontend
+    origin: process.env.CLIENT_URL || "http://localhost:3000", // allow your frontend
     credentials: true,
   })
 );
